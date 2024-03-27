c8b34b7b8d63fcaa044e8a7ce335458d5ac341df
Lakshya <lakshyak7370@gmail.com>
2024-03-27 11:58:31 +0530

    Third commit : ADD Song list and play bar

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,7 @@ console.log('Let\'s write JavaScript!!');
 
 async function getsongs() {
   try {
-    let response = await fetch("http://127.0.0.1:5500/SpotifyClone/songs/");
+    let response = await fetch("http://127.0.0.1:5500/songs/");
     if (!response.ok) {
       throw new Error('Network response was not ok.');
     }
@@ -38,7 +38,17 @@ async function main(){
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
     
     for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("%20", " ")} </li?`;
+        songUL.innerHTML = songUL.innerHTML + `<li>
+        <img class="invert" src="svg/music.svg" alt="">
+        <div class="sinfo">
+            <div> ${song.replaceAll("%20", " ")} </div>
+            <div>Lakshya </div>
+        </div>
+        <div class="playNow">
+            <span>Play Now</span>
+            <img class="invert" src="svg/play.svg" alt="">
+        </div>       
+       </li?`;
     }
 
 
